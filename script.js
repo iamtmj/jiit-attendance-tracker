62a2d58e6ab5f33daf9b5170160da2b89c5a9399
Manan Jindal <104257504+iamtmj@users.noreply.github.com>
2026-03-16 11:35:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const lastWorkingDate = new Date("2026-05-01");
+const lastWorkingDate = new Date("2026-05-04");
 // Normalize holiday dates into ISO (YYYY-MM-DD) format to ensure matches
 let holidays = [
   "2026-01-26",
